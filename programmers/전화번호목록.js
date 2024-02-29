741da3060f6e351c25389ffb073b729f55deb41a
iam454 <realseogy@gmail.com>
2024-02-29 13:42:58 +0900

    ✅ day 3: 프로그래머스 - 전화번호 목록

---
+++ programmers/전화번호목록.js
@@ -0,0 +1,13 @@
+function solution(phone_book) {
+  let answer = true;
+  phone_book.sort();
+
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      answer = false;
+      break;
+    }
+  }
+
+  return answer;
+}
