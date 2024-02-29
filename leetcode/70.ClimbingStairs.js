cd7993ab89e8e58b6208604c0c7e5ddf2973badf
iam454 <realseogy@gmail.com>
2024-02-29 14:36:38 +0900

    fix: 70.ClimbingStairs

--- leetcode/70.ClimbingStairs.js
+++ leetcode/70.ClimbingStairs.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-  dp = { 1: 1, 2: 2 };
+  let dp = { 1: 1, 2: 2 };
 
   for (let i = 3; i <= n; i++) {
     dp[i] = dp[i - 1] + dp[i - 2];
