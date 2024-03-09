86851f3240f9d0e4d4c023f8cfd53758afac38df
iam454 <realseogy@gmail.com>
2024-03-09 15:22:04 +0900

    ✅ day 12: 프로그래머스 - 단어 변환

---
+++ programmers/단어변환.js
@@ -0,0 +1,37 @@
+function solution(begin, target, words) {
+  let answer = 0;
+  let N = words.length;
+  let visited = new Array(N).fill(0);
+  let q = [];
+  q.push([begin, 0]);
+
+  while (q.length) {
+    let [w, cnt] = q.shift();
+
+    if (w === target) {
+      answer = cnt;
+      return answer;
+    }
+
+    for (let i = 0; i < N; i++) {
+      if (isConnected(w, words[i]) && visited[i] === 0) {
+        visited[i] = 1;
+        q.push([words[i], cnt + 1]);
+      }
+    }
+  }
+
+  return answer;
+}
+
+function isConnected(w1, w2) {
+  let cnt = 0;
+
+  for (let i = 0; i < w1.length; i++) {
+    if (w1[i] !== w2[i]) {
+      cnt += 1;
+    }
+  }
+
+  return cnt === 1 ? true : false;
+}
