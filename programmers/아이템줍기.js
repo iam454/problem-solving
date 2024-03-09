a6016aab6252f66b6f68e812f52ce20b2b50b771
iam454 <realseogy@gmail.com>
2024-03-09 15:53:06 +0900

    ✅ day 12: 프로그래머스 - 아이템 줍기

---
+++ programmers/아이템줍기.js
@@ -0,0 +1,55 @@
+function solution(rectangle, characterX, characterY, itemX, itemY) {
+  let board = [];
+  for (let i = 0; i < 103; i++) {
+    board[i] = new Array(103).fill(0);
+  }
+
+  characterX *= 2;
+  characterY *= 2;
+  itemX *= 2;
+  itemY *= 2;
+  let dr = rectangle.map((item) => item.map((i) => i * 2));
+
+  dr.forEach((item) => {
+    let [x1, y1, x2, y2] = item;
+
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        if (i === x1 || i === x2 || j === y1 || j === y2) {
+          if (board[i][j] === 0) {
+            board[i][j] = 1;
+          }
+        } else {
+          board[i][j] = 2;
+        }
+      }
+    }
+  });
+
+  let q = [];
+  q.push([characterX, characterY, 0]);
+  board[characterX][characterY] = -1;
+
+  let dx = [-1, 1, 0, 0];
+  let dy = [0, 0, -1, 1];
+
+  while (q.length) {
+    let [cx, cy, cp] = q.shift();
+
+    if (cx === itemX && cy === itemY) {
+      return cp / 2;
+    }
+
+    for (let i = 0; i < 4; i++) {
+      let nx = cx + dx[i];
+      let ny = cy + dy[i];
+
+      if (board[nx][ny] === 1) {
+        q.push([nx, ny, cp + 1]);
+        board[nx][ny] = -1;
+      }
+    }
+  }
+
+  return 0;
+}
