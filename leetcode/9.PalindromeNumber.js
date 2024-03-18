68c9934ddb858ff8aaa7cc9428ec04e8fad642df
iam454 <realseogy@gmail.com>
2024-03-18 15:04:18 +0900

    ✅ day 21: 리트코드 - 9. Palindrome Number.js

---
+++ leetcode/9.PalindromeNumber.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function (x) {
+  if (x < 0) {
+    return false;
+  }
+  return x === Number(x.toString().split("").reverse().join(""));
+};
