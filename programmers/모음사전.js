615af44aa8df2783865875b144af93322d6ac927
iam454 <realseogy@gmail.com>
2024-03-18 14:08:40 +0900

    ✅ day 21: 프로그래머스 - 모음사전

--- programmers/모음사전.js
+++ programmers/모음사전.js
@@ -1,22 +1,20 @@
 function solution(word) {
   let answer = 0;
-  let dic = [];
   let alpha = ["A", "E", "I", "O", "U"];
+  let dic = [];
 
   function dfs(w, cnt) {
     if (cnt > 5) {
       return;
+    } else {
+      dic.push(w);
     }
-
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < alpha.length; i++) {
       dfs(w + alpha[i], cnt + 1);
     }
-
-    dic.push(w);
   }
 
   dfs("", 0);
-  dic.sort();
 
   return dic.indexOf(word);
 }
