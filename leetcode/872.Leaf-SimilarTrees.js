c460ea08694d7968271ef0ec499b620c2079c228
iam454 <realseogy@gmail.com>
2024-04-13 18:18:21 +0900

    🤔 day 47: 리트코드 - 872. Leaf-Similar Trees

---
+++ leetcode/872.Leaf-SimilarTrees.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var leafSimilar = function (root1, root2) {
+  let arr1 = [];
+  let arr2 = [];
+
+  function dfs(node, arr) {
+    if (node) {
+      if (node.left === null && node.right === null) {
+        arr.push(node.val);
+      }
+      if (node.left !== null) {
+        dfs(node.left, arr);
+      }
+      if (node.right !== null) {
+        dfs(node.right, arr);
+      }
+    }
+  }
+
+  dfs(root1, arr1);
+  dfs(root2, arr2);
+
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+};
