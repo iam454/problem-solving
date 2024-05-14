c8815cb397f8490e7733de7afad34a132d9c3315
iam454 <realseogy@gmail.com>
2024-05-14 15:56:39 +0900

    ✅ day 78: 리트코드 - 746. Min Cost Climbing Stairs

--- leetcode/746.MinCostClimbingStairs.js
+++ leetcode/746.MinCostClimbingStairs.js
@@ -3,11 +3,12 @@
  * @return {number}
  */
 var minCostClimbingStairs = function (cost) {
-  let minCost = [cost[0], cost[1]];
+  cost.push(0);
+  let dp = [cost[0], cost[1]];
 
   for (let i = 2; i < cost.length; i++) {
-    minCost[i] = Math.min(minCost[i - 2] + cost[i], minCost[i - 1] + cost[i]);
+    dp[i] = Math.min(dp[i - 1] + cost[i], dp[i - 2] + cost[i]);
   }
 
-  return Math.min(minCost[minCost.length - 2], minCost[minCost.length - 1]);
+  return dp[dp.length - 1];
 };
