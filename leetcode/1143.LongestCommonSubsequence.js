dcfdd6c1785e790b74c645faf8e1b0b3d0860609
iam454 <realseogy@gmail.com>
2024-05-18 15:32:10 +0900

    ❌ day 82: 리트코드 - 1143. Longest Common Subsequence

---
+++ leetcode/1143.LongestCommonSubsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  let len1 = text1.length;
+  let len2 = text2.length;
+
+  let dp = Array.from({ length: len1 + 1 }, () => new Array(len2 + 1).fill(0));
+
+  for (let i = 1; i <= len1; i++) {
+    for (let j = 1; j <= len2; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  return dp[len1][len2];
+};
