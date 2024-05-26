75ed5486d234b5fc2aa83ab3dd5ae87858cd0b5e
iam454 <realseogy@gmail.com>
2024-05-26 20:29:39 +0900

    ✅ day 90: 프로그래머스 - 폰켓몬

--- programmers/폰켓몬.js
+++ programmers/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
+  let types = new Set(nums).size;
   let choices = nums.length / 2;
-  let set = new Set(nums).size;
 
-  return set > choices ? choices : set;
+  return types > choices ? choices : types;
 }
