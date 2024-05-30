6eec382f5c84cba88dbde1a68d7149e73f19122b
iam454 <realseogy@gmail.com>
2024-05-30 13:05:12 +0900

    🤔 day 94: 프로그래머스 - 베스트앨범

--- programmers/베스트앨범.js
+++ programmers/베스트앨범.js
@@ -4,24 +4,26 @@ function solution(genres, plays) {
   let N = genres.length;
 
   for (let i = 0; i < N; i++) {
-    if (obj[genres[i]]) {
-      obj[genres[i]].total += plays[i];
+    let g = genres[i];
+    let p = plays[i];
+
+    if (obj[g]) {
+      obj[g].total += p;
     } else {
-      obj[genres[i]] = {
-        total: plays[i],
+      obj[g] = {
+        total: p,
         list: [],
       };
     }
-    obj[genres[i]].list.push([i, plays[i]]);
+    obj[g].list.push([i, p]);
   }
 
-  let sorted = Object.values(obj).sort((a, b) => b.total - a.total);
-
-  sorted.forEach((i) => {
-    i.list.sort((a, b) => b[1] - a[1]);
-    answer.push(i.list[0][0]);
-    if (i.list.length > 1) {
-      answer.push(i.list[1][0]);
+  let arr = Object.values(obj).sort((a, b) => b.total - a.total);
+  arr.forEach((item) => {
+    item.list.sort((a, b) => b[1] - a[1]);
+    answer.push(item.list[0][0]);
+    if (item.list.length > 1) {
+      answer.push(item.list[1][0]);
     }
   });
 
