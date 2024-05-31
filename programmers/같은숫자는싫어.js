50799495182d4f96558b27e9d760f95ad8f2de97
iam454 <realseogy@gmail.com>
2024-05-31 14:40:12 +0900

    ✅ day 95: 프로그래머스 - 같은 숫자는 싫어

--- programmers/같은숫자는싫어.js
+++ programmers/같은숫자는싫어.js
@@ -2,9 +2,10 @@ function solution(arr) {
   let answer = [];
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] !== arr[i + 1]) {
-      answer.push(arr[i]);
+    if (answer && answer[answer.length - 1] === arr[i]) {
+      continue;
     }
+    answer.push(arr[i]);
   }
 
   return answer;
