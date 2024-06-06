c1ae077e4a200f528bd953e1932308c2012cea15
iam454 <realseogy@gmail.com>
2024-06-06 21:11:14 +0900

    ✅ day 101: 프로그래머스 - k번째수

--- programmers/k번째수.js
+++ programmers/k번째수.js
@@ -1,12 +1,11 @@
 function solution(array, commands) {
   let answer = [];
 
-  while (commands.length) {
-    let [i, j, k] = commands.shift();
-    let newArr = array.slice(i - 1, j);
-    newArr.sort((a, b) => a - b);
-    answer.push(newArr[k - 1]);
-  }
+  commands.forEach((command) => {
+    let [i, j, k] = command;
+    let cut = array.slice(i - 1, j).sort((a, b) => a - b);
+    answer.push(cut[k - 1]);
+  });
 
   return answer;
 }
