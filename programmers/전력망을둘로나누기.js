d302a0284b89471c6d8e99f8f386e805a743f70e
iam454 <realseogy@gmail.com>
2024-06-14 14:04:55 +0900

    🤔 day 109: 프로그래머스 - 전력망을 둘로 나누기

--- programmers/전력망을둘로나누기.js
+++ programmers/전력망을둘로나누기.js
@@ -1,27 +1,26 @@
 function solution(n, wires) {
   let answer = Infinity;
   let graph = Array.from({ length: n + 1 }, () => []);
-
-  wires.forEach((w) => {
-    let [s, e] = w;
-    graph[s].push(e);
-    graph[e].push(s);
+  wires.forEach((wire) => {
+    let [start, end] = wire;
+    graph[start].push(end);
+    graph[end].push(start);
   });
 
   function bfs(start, remove) {
     let cnt = 0;
-    let visited = new Array(n + 1).fill(false);
+    let visited = new Array(n + 1).fill(0);
     let q = [];
     q.push(start);
-    visited[start] = true;
+    visited[start] = 1;
 
     while (q.length) {
-      let cur = q.shift();
+      let curNode = q.shift();
 
-      for (let i = 0; i < graph[cur].length; i++) {
-        if (visited[graph[cur][i]] === false && graph[cur][i] !== remove) {
-          visited[graph[cur][i]] = true;
-          q.push(graph[cur][i]);
+      for (let i = 0; i < graph[curNode].length; i++) {
+        if (visited[graph[curNode][i]] === 0 && graph[curNode][i] !== remove) {
+          q.push(graph[curNode][i]);
+          visited[graph[curNode][i]] = 1;
         }
       }
 
@@ -31,8 +30,8 @@ function solution(n, wires) {
     return cnt;
   }
 
-  wires.forEach((w) => {
-    let [a, b] = w;
+  wires.forEach((wire) => {
+    let [a, b] = wire;
     answer = Math.min(answer, Math.abs(bfs(a, b) - bfs(b, a)));
   });
 
