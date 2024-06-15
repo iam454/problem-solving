91d519fa0da7d1a7435feaf9e4e7e1721105d4d3
iam454 <realseogy@gmail.com>
2024-06-15 20:04:26 +0900

    ✅ day 110: 프로그래머스 - 모음 사전

--- programmers/모음사전.js
+++ programmers/모음사전.js
@@ -1,16 +1,14 @@
 function solution(word) {
-  let answer = 0;
   let alpha = ["A", "E", "I", "O", "U"];
   let dic = [];
 
-  function dfs(w, cnt) {
-    if (cnt > 5) {
+  function dfs(w, len) {
+    if (len > 5) {
       return;
-    } else {
-      dic.push(w);
     }
-    for (let i = 0; i < alpha.length; i++) {
-      dfs(w + alpha[i], cnt + 1);
+    dic.push(w);
+    for (let i = 0; i < 5; i++) {
+      dfs(w + alpha[i], len + 1);
     }
   }
 
