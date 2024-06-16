dcaf088eab61e60238689f5b99c8665052b55126
iam454 <realseogy@gmail.com>
2024-06-16 18:20:00 +0900

    ✅ day 111: 프로그래머스 - 체육복

--- programmers/체육복.js
+++ programmers/체육복.js
@@ -1,22 +1,17 @@
 function solution(n, lost, reserve) {
   let have = new Array(n).fill(1);
 
-  lost.forEach((l) => {
-    have[l - 1] -= 1;
-  });
+  lost.forEach((l) => (have[l - 1] -= 1));
+  reserve.forEach((l) => (have[l - 1] += 1));
 
-  reserve.forEach((r) => {
-    have[r - 1] += 1;
-  });
-
-  for (let i = 0; i < have.length; i++) {
+  for (let i = 0; i < n; i++) {
     if (have[i] === 0) {
       if (have[i - 1] >= 2) {
+        have[i] += 1;
         have[i - 1] -= 1;
-        have[i] += 1;
       } else if (have[i + 1] >= 2) {
-        have[i + 1] -= 1;
         have[i] += 1;
+        have[i + 1] -= 1;
       }
     }
   }
