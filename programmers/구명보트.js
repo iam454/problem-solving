b0c84fc0e9ea05dd3517f9d50b2e83b7c6503578
iam454 <realseogy@gmail.com>
2024-06-19 16:30:46 +0900

    ✅ day 114: 프로그래머스 - 구명보트

--- programmers/구명보트.js
+++ programmers/구명보트.js
@@ -1,13 +1,11 @@
 function solution(people, limit) {
   let answer = 0;
   people.sort((a, b) => a - b);
-  let N = people.length;
   let l = 0;
-  let r = N - 1;
+  let r = people.length - 1;
 
-  while (l < r) {
+  while (l <= r) {
     let sum = people[l] + people[r];
-
     if (sum > limit) {
       r -= 1;
       answer += 1;
@@ -16,10 +14,6 @@ function solution(people, limit) {
       r -= 1;
       answer += 1;
     }
-  }
-
-  if (l === r) {
-    answer += 1;
   }
 
   return answer;
