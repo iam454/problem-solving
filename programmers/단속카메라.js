e91f930d62b046dfddc8547db36f67000886d8a1
iam454 <realseogy@gmail.com>
2024-06-20 13:29:31 +0900

    🤔 day 115: 프로그래머스 - 단속카메라

--- programmers/단속카메라.js
+++ programmers/단속카메라.js
@@ -1,13 +1,13 @@
 function solution(routes) {
   let answer = 0;
-  routes.sort((a, b) => a[1] - b[1]);
   let camera = -30001;
+  routes.sort((a, b) => a[1] - b[1]);
   routes.forEach((route) => {
     let [enter, leave] = route;
 
     if (enter > camera) {
-      answer += 1;
       camera = leave;
+      answer += 1;
     }
   });
 
