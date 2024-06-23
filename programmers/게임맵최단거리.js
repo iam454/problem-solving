8ce0ea09bef08d24b9defd8653471f1006b6fa86
iam454 <realseogy@gmail.com>
2024-06-23 11:10:39 +0900

    ✅ day 118: 프로그래머스 - 게임 맵 최단거리

--- programmers/게임맵최단거리.js
+++ programmers/게임맵최단거리.js
@@ -6,15 +6,17 @@ function solution(maps) {
   for (let i = 0; i < n; i++) {
     visited[i] = new Array(m).fill(0);
   }
-  let dx = [-1, 1, 0, 0];
-  let dy = [0, 0, -1, 1];
 
   let q = [];
   q.push([0, 0, 1]);
   visited[0][0] = 1;
 
+  let dx = [-1, 1, 0, 0];
+  let dy = [0, 0, -1, 1];
+
   while (q.length) {
     let [x, y, cnt] = q.shift();
+
     if (x === n - 1 && y === m - 1) {
       answer = cnt;
       return answer;
@@ -25,9 +27,9 @@ function solution(maps) {
       let ny = y + dy[i];
 
       if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
-        if (maps[nx][ny] === 1 && visited[nx][ny] === 0) {
-          visited[nx][ny] = 1;
+        if (visited[nx][ny] === 0 && maps[nx][ny] === 1) {
           q.push([nx, ny, cnt + 1]);
+          visited[nx][ny] = 1;
         }
       }
     }
