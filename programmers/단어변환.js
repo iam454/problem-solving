68e55a5f4bf20dd8f83e252fba398de8893c193c
iam454 <realseogy@gmail.com>
2024-06-24 10:36:33 +0900

    ✅ day 119: 프로그래머스 - 단어 변환

--- programmers/단어변환.js
+++ programmers/단어변환.js
@@ -2,6 +2,7 @@ function solution(begin, target, words) {
   let answer = 0;
   let N = words.length;
   let visited = new Array(N).fill(0);
+
   let q = [];
   q.push([begin, 0]);
 
@@ -14,7 +15,7 @@ function solution(begin, target, words) {
     }
 
     for (let i = 0; i < N; i++) {
-      if (isConnected(w, words[i]) && visited[i] === 0) {
+      if (visited[i] === 0 && isConnected(w, words[i])) {
         visited[i] = 1;
         q.push([words[i], cnt + 1]);
       }
@@ -27,7 +28,7 @@ function solution(begin, target, words) {
 function isConnected(w1, w2) {
   let cnt = 0;
 
-  for (let i = 0; i < w1.length; i++) {
+  for (let i = 0; i < w2.length; i++) {
     if (w1[i] !== w2[i]) {
       cnt += 1;
     }
