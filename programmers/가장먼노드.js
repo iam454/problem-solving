90e378281f8ce555de0d0734804604ec0d6c859e
iam454 <realseogy@gmail.com>
2024-06-27 14:57:19 +0900

    ✅ day 122: 프로그래머스 - 가장 먼 노드

--- programmers/가장먼노드.js
+++ programmers/가장먼노드.js
@@ -1,29 +1,29 @@
 function solution(n, edge) {
   let graph = Array.from({ length: n + 1 }, () => []);
-  let visited = new Array(n + 1).fill(0);
-
-  edge.forEach((e) => {
-    let [start, end] = e;
-    graph[start].push(end);
-    graph[end].push(start);
+  edge.forEach((item) => {
+    let [s, e] = item;
+    graph[s].push(e);
+    graph[e].push(s);
   });
 
+  let visited = new Array(n + 1).fill(0);
   let q = [];
-  q.push([1, graph[1]]);
+  q.push([graph[1], 1]);
   visited[1] = 1;
 
   while (q.length) {
-    let [cnt, edges] = q.shift();
+    let [edges, cnt] = q.shift();
 
     for (let i = 0; i < edges.length; i++) {
-      if (visited[edges[i]] === 0) {
-        visited[edges[i]] = cnt + 1;
-        q.push([cnt + 1, graph[edges[i]]]);
+      let next = edges[i];
+      if (visited[next] === 0) {
+        visited[next] = cnt + 1;
+        q.push([graph[next], cnt + 1]);
       }
     }
   }
 
   let max = Math.max(...visited);
 
-  return visited.filter((v) => v === max).length;
+  return visited.filter((c) => c === max).length;
 }
