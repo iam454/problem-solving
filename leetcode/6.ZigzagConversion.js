2eeb1c8214d773bfd798a227518f7acbccefaf67
iam454 <realseogy@gmail.com>
2024-06-30 18:04:20 +0900

    🤔 day 125: 리트코드 - 6. Zigzag Conversion

---
+++ leetcode/6.ZigzagConversion.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function (s, numRows) {
+  let ans = new Array(numRows).fill().map(() => []);
+
+  let idx = 0;
+  let next = 1;
+
+  for (let i = 0; i < s.length; i++) {
+    ans[idx].push(s[i]);
+    if (idx === 0) {
+      next = 1;
+    } else if (idx === numRows - 1) {
+      next = -1;
+    }
+    idx += next;
+  }
+
+  return ans.map((i) => i.join("")).join("");
+};
